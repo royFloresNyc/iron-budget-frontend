da017fdc3366a9b353d0418abfb0d15629b93ae3
royFloresNyc <royfloresnyc@gmail.com>
2020-10-21 19:45:02 -0400

    Add color styling on Amount based on transaction type

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -17,11 +17,19 @@ const Transaction = ({ categories, transaction, deleteHandler, editClickHandler 
         return categories.find(cat => transaction.category_id === cat.id).name
     }
 
+    const creditStyle = {
+        color: "#14cc60"
+    }
+
+    const debitStyle = {
+        color: "#d83b00"
+    }
+
     return (<div className="transaction" onMouseEnter={mouseEnterHandler} onMouseLeave={mouseExitHandler}>
         <div className="date">{transaction.t_date}</div>
         <div className="name">{transaction.name}</div>
         <div className="category-name">{getCategoryName(transaction)}</div>
-        <div className="amount">${transaction.amount}</div>
+        <div className="amount" style={transaction.transaction_type_id === 1 ? creditStyle : debitStyle}>${transaction.amount}</div>
         <div className="t-btn">
             <div className="edit-btn" onClick={() => editClickHandler(transaction)}>
                 { showButtons ? <img className="icon-btn" src={editIcon} alt="edit icon" /> : null }
