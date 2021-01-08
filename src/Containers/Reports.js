e0dc7de741e89ac909922a1c81c76d1e56cc6214
royFloresNyc <royfloresnyc@gmail.com>
2021-01-07 19:30:22 -0500

    Code CleanUp

--- src/Containers/Reports.js
+++ src/Containers/Reports.js
@@ -12,47 +12,28 @@ import Spinner from 'react-bootstrap/Spinner'
  
 
 class Reports extends React.Component {
-//--State--------------------------------------------------------
-    // state = {
-    //     userInfo: []
-    // }
-//--Fetch--------------------------------------------------------
-    // componentDidMount = () => {
-    //     fetch(`http://localhost:3000/users/1`)
-    //     .then(resp => resp.json())
-    //     .then(userData => this.setState({userInfo: userData}))
-    // }
-//---------------------------------------------------------------
+
     render() {
         return(
             <div>
-                {/* <Switch>
-                    <Route path='/reports/basic_finances' render={() => this.props.user.transactions ? <BasicFinances info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <h1>"LOADING"</h1> } />
-                    <Route path='/reports/income' render={() => this.props.user.transactions ? <Income info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <h1>"LOADING"</h1> } />
-                    <Route path='/reports/expenditure' render={() => this.props.user.transactions ? <Expenditure info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <h1>"LOADING"</h1> } />
-                    <Route path='/reports/budget_comparison' render={() => this.props.user.transactions ?  <BudgetComparison info={this.props.user} periods={this.props.user.transacts_by_period}/> : <h1>"LOADING"</h1> }/>
-                    <Route path='/reports/projection' render={() => this.props.user.transactions ? <Projection info={this.props.user} periods={this.props.user.transacts_by_period}/> : <h1>"LOADING"</h1> }/>
-                    
-                </Switch> */}
-            <ReportsNav/>
-            <Switch>
-                <Route path='/reports/income' render={() => this.props.user.transactions ? <Income info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-                </Spinner>} />
-                                <Route path='/reports/expenditure' render={() => this.props.user.transactions ? <Expenditure info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-                </Spinner>} />
-                                <Route path='/reports/budget_comparison' render={() => this.props.user.transactions ? <BudgetComparison info={this.props.user} periods={this.props.user.transacts_by_period} createBudget={this.props.createBudget}/> : <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-                </Spinner>}/>
-                                <Route path='/reports/projection' render={() => this.props.user.transactions ? <Projection info={this.props.user} periods={this.props.user.transacts_by_period}/> : <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-                </Spinner>}/>
-                <Route path={'/reports/' || '/reports/basic_finances'} render={() => this.props.user.transactions ? <BasicFinances info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> : <Spinner animation="border" role="status">
-                <span className="sr-only">Loading...</span>
-                </Spinner>} />
-            </Switch>
-            
+                <ReportsNav/>
+                <Switch>
+                    <Route path='/reports/income' render={() => this.props.user.transactions ? 
+                        <Income info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> 
+                        : <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>} />
+                    <Route path='/reports/expenditure' render={() => this.props.user.transactions ? 
+                        <Expenditure info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> 
+                        : <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>} />
+                    <Route path='/reports/budget_comparison' render={() => this.props.user.transactions ? 
+                        <BudgetComparison info={this.props.user} periods={this.props.user.transacts_by_period} createBudget={this.props.createBudget}/> 
+                        : <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>}/>
+                    <Route path='/reports/projection' render={() => this.props.user.transactions ? 
+                        <Projection info={this.props.user} periods={this.props.user.transacts_by_period}/> 
+                        : <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>}/>
+                    <Route path={'/reports/' || '/reports/basic_finances'} render={() => this.props.user.transactions ? 
+                        <BasicFinances info={this.props.user.transactions} periods={this.props.user.transacts_by_period}/> 
+                        : <Spinner animation="border" role="status"><span className="sr-only">Loading...</span></Spinner>} />
+                </Switch>
             </div>
         )
     }
